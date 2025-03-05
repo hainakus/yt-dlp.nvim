a9623cf010ea5c8d364c0fe54b0670b6336198b5
hainakus <djhainakosurge@gmail.com>
2025-03-05 21:50:52 +0000

    Update yt-dlp.lua

--- yt-dlp.lua
+++ yt-dlp.lua
@@ -1,26 +1,29 @@
+local yt_dlp = {}
+
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local conf = require("telescope.config").values
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
+
 local playlist_file = "/tmp/nvim_yt_playlist.txt"
 
 -- Ensure playlist file exists
 os.execute("touch " .. playlist_file)
 
-local function add_to_playlist()
-    local url = vim.fn.input("YouTube URL: ")
+function yt_dlp.add_to_playlist()
+    local url = vim.fn.input("🎵 YouTube URL: ")
     if url == "" then return end
     os.execute("yt-dlp -f bestaudio --get-title " .. url .. " >> " .. playlist_file)
     os.execute("echo " .. url .. " >> " .. playlist_file)
-    print("Added to playlist!")
+    print("✅ Added to playlist!")
 end
 
-local function play_song(song)
+function yt_dlp.play_song(song)
     os.execute("mpv --no-video --quiet " .. song .. " &")
 end
 
-local function show_playlist()
+function yt_dlp.show_playlist()
     local playlist = {}
     for line in io.lines(playlist_file) do
         table.insert(playlist, line)
@@ -35,7 +38,7 @@ local function show_playlist()
                 local selection = action_state.get_selected_entry()
                 actions.close(prompt_bufnr)
                 if selection then
-                    play_song(selection[1])
+                    yt_dlp.play_song(selection[1])
                 end
             end
             map("i", "<CR>", select_song)
@@ -45,5 +48,9 @@ local function show_playlist()
     }):find()
 end
 
-vim.api.nvim_create_user_command("YTAdd", add_to_playlist, {})
-vim.api.nvim_create_user_command("YTPlaylist", show_playlist, {})
+function yt_dlp.setup()
+    vim.api.nvim_create_user_command("YTAdd", yt_dlp.add_to_playlist, {})
+    vim.api.nvim_create_user_command("YTPlaylist", yt_dlp.show_playlist, {})
+end
+
+return yt_dlp
